dcd33dfddab4183c6bb49a17c0a93d38a0bdfa18
Atienn <70545238+Atienn@users.noreply.github.com>
2021-09-27 20:01:31 -0400

    Expanded Upon Game States

--- Project Warzone - D02/GameEngine.cpp
+++ Project Warzone - D02/GameEngine.cpp
@@ -7,3 +7,34 @@ void SwitchState(GameState& newState) {
 	current = &newState;
 	current->Setup();
 }
+
+void GameSetup() {
+	using namespace States;
+
+	//Initialize all game states.
+	GameState* states[8] = {
+		new Start,
+		new LoadMap,
+		new ValidMap,
+		new AddPlayers,
+		new AssignReinf,
+		new IssueOrders,
+		new ExecOrders,
+		new GameOver
+	};
+	
+	//Establish links between states.
+	//Doesn't work right now because 'arrays of abstract functions are not allowed'.
+	states[0]->links = new GameState[1]{ *states[1] };
+	states[1]->links = new GameState[2]{ *states[1], *states[2] };
+	states[2]->links = new GameState[1]{ *states[3] };
+	states[3]->links = new GameState[2]{ *states[3], *states[4] };
+	states[4]->links = new GameState[1]{ *states[5] };
+	states[5]->links = new GameState[2]{ *states[5], *states[6] };
+	states[6]->links = new GameState[3]{ *states[6], *states[4], *states[7] };
+	states[7]->links = new GameState[1]{ *states[0] };
+
+	//Set the starting game state and call its setup function.
+	current = states[0];
+	current->Setup();
+}
--- Project Warzone - D02/GameEngine.h
+++ Project Warzone - D02/GameEngine.h
@@ -7,24 +7,127 @@ namespace Engine {
 	//Abstract class representing a state the game can be in.
 	class GameState {
 	public:
-		virtual void Setup();
-		virtual void Exit();
+		virtual void Setup() = 0;
+		virtual void Exit() = 0;
+
+		GameState *links;
+		std::string* commands;
 	};
+
 
 	//Holds the current GameState.
 	GameState* current;
+	
 	//Free function to switch from one state to another.
 	void SwitchState(GameState&);
 
-	//Example of implementation of GameState abstract class.
-	class StartState : GameState {
-		void Setup();
-		void Exit();
+	//Creates an instance of each game state and establishes the connections between each.
+	void GameSetup();
 
-		//Constructor.
-		StartState() { }
 
-		//Destructor.
-		~StartState() { }
-	};
+	namespace States {
+
+		//Example of implementation of GameState abstract class.
+		class Example : public GameState {
+			void GameState::Setup();
+			void GameState::Exit();
+
+			//Constructor.
+			Example() {
+				GameState::commands = new std::string[1]{ "command" };
+			}
+
+			//Destructor.
+			~Example() { }
+		};
+
+
+		class Start : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			Start() {
+				GameState::commands = new std::string[1]{ "loadmap" };
+			}
+			~Start() { }
+		};
+
+		class LoadMap : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			LoadMap() {
+				GameState::commands = new std::string[2]{ "loadmap", "validatemap" };
+			}
+			~LoadMap() { }
+		};
+
+		class ValidMap : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			ValidMap() {
+				GameState::commands = new std::string[1]{ "addplayers" };
+			}
+			~ValidMap() { }
+		};
+
+		class AddPlayers : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			AddPlayers() {
+				GameState::commands = new std::string[2]{ "addplayers", "assigncountries" };
+			}
+			~AddPlayers() { }
+		};
+
+		class AssignReinf : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			AssignReinf() {
+				GameState::commands = new std::string[1]{ "issueorder" };
+			}
+			~AssignReinf() { }
+		};
+
+		class IssueOrders : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			IssueOrders() {
+				GameState::commands = new std::string[2]{ "issueorder", "endissueorders" };
+			}
+			~IssueOrders() { }
+		};
+
+		class ExecOrders : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			ExecOrders() {
+				GameState::commands = new std::string[3]{ "execorder", "endexecorders", "win" };
+			}
+			~ExecOrders() { }
+		};
+
+		class GameOver : public GameState {
+		public:
+			void GameState::Setup();
+			void GameState::Exit();
+
+			GameOver() {
+				GameState::commands = new std::string[2]{ "play", "end" };
+			}
+			~GameOver() { }
+		};
+	}
 }
