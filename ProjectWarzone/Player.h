ca7276e4f52b3d511f44bd5f63c81491cd49edb5
DawiDude <49909629+DawiDude@users.noreply.github.com>
2021-11-30 19:37:47 -0500

    Update Player.h

--- ProjectWarzone/Player.h
+++ ProjectWarzone/Player.h
@@ -6,6 +6,7 @@
 #include "Map.h"
 #include "Orders.h"
 #include "Cards.h"
+#include "PlayerStrategies.h"
 
 using namespace std;
 
@@ -86,6 +87,8 @@ public:
 	bool ordersComplete;
 	bool havePlayedCard;
 	void setHand(Hand* hand) { this->hand = hand; }
+	PlayerStrategy* getStrategy() { return strat; }
+	void setStrategy(PlayerStrategy* strat) { this->strat = strat; }
 
 	int getTerritorySize();
 	int getPlayOrder();
@@ -98,7 +101,7 @@ private:
 	int numOfReinF;
 	vector<Player*> negotiate; //vector of players that cannot be attacked. Clear after every turn
 	bool drawn; //player has drawn card. Set to false after every turn
-
+	PlayerStrategy* strat;
 	int playOrder;
 
 public:
