7780e0d8f2ce1fa5688af8e8dc2c1d06e056c442
DawiDude <49909629+DawiDude@users.noreply.github.com>
2021-11-30 19:39:20 -0500

    Update Player.cpp

--- ProjectWarzone/Player.cpp
+++ ProjectWarzone/Player.cpp
@@ -20,112 +20,20 @@ Player& Player::operator=(const Player& player) {
 
 vector<territory*> Player::toDefend(Map m) 
 {
-	vector<territory*> territoriesToDefend;
-	//TLDR the player assigns a random number of territories to defend
-	for (int i = 0; i < rand() % (getTerritories().size() - 1, 1); i++)
-	{
-		territoriesToDefend.push_back(getTerritories()[rand() % (getTerritories().size())]);
-	}
-	//make randomizer for the to defend generating
-	return territoriesToDefend;
+	strat->toDefend(m);
 }
 
 vector<territory*> Player::toAttack(Map m) {
-	vector<territory*> territoriesToAttack;
-	territory *temp;
-	int limit = rand() % (territories.size()/2);
-	int counter = 0;
-	int i = 0;
-	while (counter < limit)
-	{
-		temp = getTerritories()[rand() % (getTerritories().size())];
-		for (int i = 0; i < temp->getNumOfBorders(); i++);
-		{
-			if (m.getTerritory(temp->getBorderID(i))->getOwner()->getName() != getName())
-			{
-				territoriesToAttack.push_back(temp);
-				break;
-			}
-		}
-		counter++;
-	}
-	//make randomizer for the to attack generating
-	return territoriesToAttack;
+	strat->toAttack(m);
 }
 
+<<<<<<< HEAD
 void Player::issueOrder(int& reinf, Map m, vector<territory*> attack, vector<territory*> defend, Player* me, Deck* deck, int counter, Player neutral) 
+=======
+void Player::issueOrder(int reinf, Map m, vector<territory*> attack, vector<territory*> defend, Player* me, Deck* deck, Player* neutral, vector<Player*> participants) 
+>>>>>>> dfce499c83c447c52e985069db80540c54a4c363
 {
-	bool goOn = false;
-	int armies = rand() % (reinf / 2 + 1) + (reinf / 2 + 1);
-	reinf -= armies;
-	//this first if is to ensure that the character doesn't
-	if (ordersComplete)
-	{
-		return;
-	}
-	if (reinf > 0)
-	{
-		//reinfordement logic
-		if (counter < defend.size())
-		{
-			addOrder(new Deploy(armies, me, defend[counter]));
-		}
-		else
-		{
-			addOrder(new Deploy(armies, me, m.getTerritory(attack[counter]->getBorderID(0))));
-		}
-	}
-	else if(!havePlayedCard && getHand()->size()!=0)
-	{
-		//play a card and remove it from hand
-		//must make the card making an order or apropriate type
-		switch ((getHand()->getCards())[0]->getCardType())
-		{
-		case (Card::bomb):
-			addOrder(new Bomb(me, attack[counter - defend.size()]));
-			break;
-		case (Card::reinforcement):
-			addReinF(5);
-			break;
-		case (Card::blockade):
-			addOrder(new Blockade(me, neutral, defend[counter]));
-			break;
-		case (Card::airlift):
-			//at the moment the planes are only good enough for 5 ppl
-			addOrder(new Airlift(5, me, defend[0], attack[counter - defend.size()]));
-			break;
-		case (Card::diplomacy):
-			addOrder(new Negotiate(me, attack[counter - defend.size()]->getOwner()));
-			break;
-		case (Card::EMPTY):
-			break;
-		}
-		//removes card form hand
-		(hand->getCards())[0]->play(0);
-		havePlayedCard = true;
-	}
-	else if (counter - defend.size() <= attack.size())
-	{
-		if (counter >= defend.size())
-		{
-			addOrder(new Advance(m.getTerritory(attack[counter - defend.size()]->getBorderID(0))->getArmies() - 1,
-				me,
-				m.getTerritory(attack[counter - defend.size()]->getBorderID(0)), 
-				attack[counter - defend.size()], deck));
-		}
-		else
-		{
-			addOrder(new Advance(m.getTerritory(defend[counter]->getBorderID(0))->getArmies() - 1,
-				me,
-				m.getTerritory(defend[counter]->getBorderID(0)),
-				defend[counter], deck));
-		}
-		//move logic
-	}
-	else
-	{
-		ordersComplete = true;
-	}
+	strat->issueOrder(reinf, m, attack, defend, me, deck, neutral, participants);
 }
 
 vector<Order*> Player::getOrders()
